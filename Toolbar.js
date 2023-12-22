739a58b63ae0818b77ac045d80bd88f41cee91ae
swe30 <54991667+swe30@users.noreply.github.com>
2023-12-23 02:02:11 +0530

    Create  Toolbar.js

---
+++ Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Toolbar({ selectedTool, setSelectedTool, annotationsVisible, setAnnotationsVisible }) {
+  // Implement toolbar buttons and functionality
+  // ...
+
+  return (
+    <div>
+      {/* Render toolbar buttons here */}
+    </div>
+  );
+}
+
+export default Toolbar;
